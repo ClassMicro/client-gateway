181bad0eaa1e3c74a21f361f8e8fa1dd240594a9
juan david montenegro <davidmontenegro-b@hotmail.com>
2025-08-24 13:27:08 -0500

    cambios

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -2,20 +2,22 @@ import { Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { NATS_SERVICE } from 'src/config';
+import { NatsModule } from 'src/transports/nats.module';
 // import { envs, ORDER_SERVICE } from 'src/config';
 
 @Module({
   imports : [
-    ClientsModule.register([
-      {
-        name : NATS_SERVICE,
-        transport : Transport.NATS,
-        options :{
-          // host : envs.ordersmicroserviceHost,
-          // port : envs.ordersmicroservicePort
-        }
-      }
-    ])
+    NatsModule
+    // ClientsModule.register([
+    //   {
+    //     name : NATS_SERVICE,
+    //     transport : Transport.NATS,
+    //     options :{
+    //       // host : envs.ordersmicroserviceHost,
+    //       // port : envs.ordersmicroservicePort
+    //     }
+    //   }
+    // ])
   ],
   controllers: [OrdersController],
   providers: [],
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -2,20 +2,22 @@ import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { envs, NATS_SERVICE } from 'src/config';
+import { NatsModule } from 'src/transports/nats.module';
 
 @Module({
+  imports :[NatsModule],
   // definimos nuestro microservicio y lo enrutamos asi el controlador de peticiones de productos
   // se conecta a este microservicio
-  imports:[ ClientsModule.register([{
+  // imports:[ ClientsModule.register([{
 
-    // creamos la constante PRODUCT_SERVICE que es la clave de nuestro microservicio
-    name : NATS_SERVICE,
-    transport: Transport.NATS,
-    options :{
-        // host : envs.productsMicroserviceHost,
-        // port : envs.productsMicroservicePort,
-        servers : envs.NATS_SERVERS    }
-  }])],
+  //   // creamos la constante PRODUCT_SERVICE que es la clave de nuestro microservicio
+  //   name : NATS_SERVICE,
+  //   transport: Transport.NATS,
+  //   options :{
+  //       // host : envs.productsMicroserviceHost,
+  //       // port : envs.productsMicroservicePort,
+  //       servers : envs.NATS_SERVERS    }
+  // }])],
   controllers: [ProductsController],
   providers: [],
 })
